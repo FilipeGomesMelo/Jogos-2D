3e9151c1543d47d3eacdeef5d37ea2c172b5b622
Rodrigo Mesel <romesel1@gmail.com>
2025-03-12 14:55:27 -0300

    bit indicator on the bottom side

--- Action RPG Resources/UI/BitCounterHud.gd
+++ Action RPG Resources/UI/BitCounterHud.gd
@@ -1,7 +1,7 @@
 extends Control
 
 const CIRCLE_SPEED = 1  # Velocidade do círculo se movendo para o coração
-const START_RADIUS = 150  # Distância inicial dos círculos
+const START_RADIUS = 100  # Distância inicial dos círculos
 export var circle_texture: Texture  # Defina a textura do círculo no editor
 
 onready var beats_container = $BeatsContainer
@@ -23,23 +23,36 @@ func _on_Conductor_quarter_passed(beat):
 	circle.texture = circle_texture
 	circle.rect_size = Vector2(10,10)  # Tamanho do círculo
 	circle.rect_scale = Vector2(0.2, 0.2)
+	
+	var circle2 = TextureRect.new()
+	circle2.texture = circle_texture
+	circle2.rect_size = Vector2(10,10)  # Tamanho do círculo
+	circle2.rect_scale = Vector2(0.2, 0.2)
 
 	# Define a posição inicial do círculo ao redor da endzone
-	circle.rect_position = Vector2(endzone.rect_position.x - START_RADIUS, 0) 
+	circle.rect_position = Vector2(endzone.rect_position.x - START_RADIUS, endzone.rect_position.y) 
 	beats_container.add_child(circle)
+	
+	circle2.rect_position = Vector2(endzone.rect_position.x + START_RADIUS, endzone.rect_position.y) 
+	beats_container.add_child(circle2)
 
 	# Criar um Tween para animar o círculo até o coração
 	var tween = Tween.new()
+	var tween2 = Tween.new()	
 	add_child(tween)
-	tween.interpolate_property(circle, "rect_position", circle.rect_position, endzone.rect_position, 1.5, Tween.TRANS_SINE, Tween.EASE_IN)
-	
+	add_child(tween2)
+	tween.interpolate_property(circle, "rect_position", circle.rect_position, endzone.rect_position, CIRCLE_SPEED, Tween.TRANS_SINE, Tween.TRANS_LINEAR)
+	tween2.interpolate_property(circle2, "rect_position", circle2.rect_position, endzone.rect_position, CIRCLE_SPEED, Tween.TRANS_SINE, Tween.TRANS_LINEAR)	
 	# Conectar diretamente o sinal de finalização da animação
 	tween.connect("tween_completed", self, "_on_tween_complete", [circle, tween])
+	tween2.connect("tween_completed", self, "_on_tween_complete", [circle, tween])
 
 	tween.start()
+	tween2.start()
+	
 
-func _on_tween_complete(tween, object_id):
+func _on_tween_complete():
 	# Remover o círculo e o Tween da árvore
-	if object_id:
-		object_id.queue_free()
-	tween.queue_free()  # Também libera o tween
+	print('AAAAAAAAAAAAAaa')
+	#if object:
+	#	object.queue_free()
