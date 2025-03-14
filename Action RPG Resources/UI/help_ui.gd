fcd1c6430941b76fb4a1c67621faea307aba9fa0
RodrigoMesel <romesel1@gmail.com>
2025-03-14 18:09:19 -0300

    feat: heart animation

--- Action RPG Resources/UI/help_ui.gd
+++ Action RPG Resources/UI/help_ui.gd
@@ -2,11 +2,11 @@ extends Control
 
 var hearts = 4 setget set_hearts
 var max_hearts = 4 setget set_max_hearts
-var position = false
 
 onready var heart_ui_full = $HeartUIFull
 onready var heart_ui_empty = $HeartUIEmpty
-
+var bigHeart = false
+var tween = Tween.new()
 
 func set_hearts(value):
 	hearts = clamp(value, 0, max_hearts)
@@ -20,28 +20,25 @@ func set_hearts(value):
 func set_max_hearts(value):
 	max_hearts = max(value, 1)
 	self.hearts = min(hearts, max_hearts)
-	
-	
-func changePosition():
-	heart_ui_full.rect_position = Vector2(0, 0)
-	heart_ui_empty.rect_position = Vector2(0, 0)
-
+		
 func _ready():
 	self.max_hearts = PlayerStats.max_health
 	self.hearts = PlayerStats.health
+	add_child(tween)
 	PlayerStats.connect("health_changed", self, "set_hearts")
 	PlayerStats.connect("max_health_changed", self, "set_max_hearts")
 	if Conductor:
 		Conductor.connect("quarter_passed", self, "_on_Conductor_quarter_passed")
 
-
 func _on_Conductor_quarter_passed(beat):
-	if !position:
-		heart_ui_full.rect_scale= Vector2(1.1, 1.1)
-		heart_ui_empty.rect_scale= Vector2(1.1, 1.1)
-		heart_ui_empty.rect_position = Vector2(2, 10)
-	else :
-		heart_ui_empty.rect_position = Vector2(2, 8)
-		heart_ui_full.rect_scale= Vector2(1, 1)
-		heart_ui_empty.rect_scale= Vector2(1, 1)
-	position = !position
+		
+	if bigHeart:
+		tween.interpolate_property(heart_ui_full, "rect_scale", Vector2(1.2, 1.2), Vector2(1, 1), 1)
+		tween.interpolate_property(heart_ui_empty, "rect_scale", Vector2(1.2, 1.2), Vector2(1, 1), 1)
+	else:
+		tween.interpolate_property(heart_ui_full, "rect_scale", Vector2(1, 1), Vector2(1.2, 1.2), 1)
+		tween.interpolate_property(heart_ui_empty, "rect_scale", Vector2(1, 1), Vector2(1.2, 1.2), 1)
+	bigHeart = !bigHeart
+	
+	tween.start()
+
